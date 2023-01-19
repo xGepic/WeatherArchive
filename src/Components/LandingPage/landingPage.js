f196d5032e7789a98ef5e327095bcaa1bf203226
Steve <simanek.s@gmx.at>
2023-01-19 16:10:46 +0100

    Beautify

--- src/Components/LandingPage/landingPage.js
+++ src/Components/LandingPage/landingPage.js
@@ -1,25 +1,37 @@
 import React from "react";
 import "./style.css";
 
+const Titel = () => {
+  return (
+    <div id="titeldiv">
+      <h1 id="titel">Weather Archive</h1>
+    </div>
+  );
+};
+
+const Searchbar = () => {
+  return (
+    <div id="searchBarDiv">
+      <form action="/" method="get">
+        <input
+          type="text"
+          id="search-bar"
+          placeholder="Search the Archive"
+          name="s"
+        />
+        <button type="submit" id="searchButton">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
 const LandingPage = () => {
   return (
     <>
-      <div id="titeldiv">
-        <h1 id="titel">Weather Archive</h1>
-      </div>
-      <div id="searchBarDiv">
-        <form action="/" method="get">
-          <input
-            type="text"
-            id="search-bar"
-            placeholder="Search the Archive"
-            name="s"
-          />
-          <button type="submit" id="searchButton">
-            Search
-          </button>
-        </form>
-      </div>
+      <Titel></Titel>
+      <Searchbar></Searchbar>
     </>
   );
 };
