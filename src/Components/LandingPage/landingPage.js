5ccc6aa4c5fc6d2e11c17a0e327adf75c14499c1
Steve <simanek.s@gmx.at>
2023-01-20 21:48:43 +0100

    API Call with Random API works

--- src/Components/LandingPage/landingPage.js
+++ src/Components/LandingPage/landingPage.js
@@ -1,36 +1,70 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./style.css";
 
 const LandingPage = () => {
   const [searchInput, getSearchInput] = useState("");
+  const [imageSrc, setImageSrc] = useState(null);
 
   const MakeLamdaCall = () => {
-    alert(searchInput);
+    axios
+      .get(
+        "https://random.imagecdn.app/v1/image?width=500&height=150&category=" +
+          { searchInput } +
+          "&format=image",
+        {
+          responseType: "arraybuffer",
+        }
+      )
+      .then((response) => {
+        const base64 = btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ""
+          )
+        );
+        const src = `data:${response.headers["content-type"]};base64,${base64}`;
+        setImageSrc(src);
+      })
+      .catch((error) => console.log(error));
   };
 
-  return (
-    <>
-      <div id="titeldiv">
-        <h1 id="titel">Weather Archive</h1>
+  function RenderPage() {
+    return (
+      <div>
+        <div id="titeldiv">
+          <h1 id="titel">Weather Archive</h1>
+        </div>
+        <div id="wrapper">
+          <div id="searchBarDiv">
+            <form action="/" method="get">
+              <input
+                type="text"
+                id="search-bar"
+                placeholder="Search the Archive"
+                name="s"
+                value={searchInput}
+                onChange={(e) => getSearchInput(e.target.value)}
+              />
+            </form>
+            <div id="buttonDiv">
+              <button onClick={MakeLamdaCall} id="searchButton">
+                Search
+              </button>
+            </div>
+          </div>
+          <div id="imgDiv">
+            {imageSrc && (
+              <div>
+                <img src={imageSrc} alt="Image from API" />
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-      <div id="searchBarDiv">
-        <form action="/" method="get">
-          <input
-            type="text"
-            id="search-bar"
-            placeholder="Search the Archive"
-            name="s"
-            value={searchInput}
-            onChange={(e) => getSearchInput(e.target.value)}
-          />
-          <button type="submit" id="searchButton" onClick={MakeLamdaCall}>
-            Search
-          </button>
-        </form>
-      </div>
-    </>
-  );
+    );
+  }
+  return RenderPage();
 };
 
 export default LandingPage;
