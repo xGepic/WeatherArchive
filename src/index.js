058978932bc7b4021c311df12bf00c5ac0b0b97b
Steve <simanek.s@gmx.at>
2023-01-20 20:21:51 +0100

    MyComponent = testing, landingpage almost done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import LandingPage from "./Components/LandingPage/landingPage";
+import MyComponent from "./Components/LandingPage/MyComponent";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
