ac099daad9a8eb8fdd838c9d70c5d1441b6e7b51
Steve <simanek.s@gmx.at>
2023-01-22 12:38:31 +0100

    goback btn + api call for multiple images

--- src/Components/LandingPage/landingPage.js
+++ src/Components/LandingPage/landingPage.js
@@ -1,76 +1,58 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./style.css";
 
 const LandingPage = () => {
-  const [searchInput, getSearchInput] = useState("");
-  const [imageSrc, setImageSrc] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [images, setImages] = useState([]);
 
-  const MakeLamdaCall = () => {
-    axios
-      .get(
-        "https://random.imagecdn.app/v1/image?width=500&height=150&category=" +
-          { searchInput } +
-          "&format=image",
-        {
-          responseType: "arraybuffer",
-        }
-      )
-      .then((response) => {
-        const base64 = btoa(
-          new Uint8Array(response.data).reduce(
-            (data, byte) => data + String.fromCharCode(byte),
-            ""
-          )
-        );
-        const src = `data:${response.headers["content-type"]};base64,${base64}`;
-        setImageSrc(src);
-      })
-      .catch((error) => console.log(error));
+  const handleSearch = async () => {
+    const response = await axios.get(`https://your-api-endpoint.com`, {
+      params: {
+        search: searchTerm,
+      },
+    });
+    setImages(response.data);
   };
 
-  function RenderPage() {
-    return (
-      <div>
-        <div id="titeldiv">
-          <h1 id="titel">Weather Archive</h1>
+  return (
+    <div>
+      <div id="titeldiv">
+        <h1 id="titel">Weather Archive</h1>
+      </div>
+      <div id="wrapper">
+        <div id="searchBarDiv">
+          <input
+            type="text"
+            id="search-bar"
+            placeholder="Search the Archive"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <div id="buttonDiv">
+            <button onClick={handleSearch} id="searchButton">
+              Search
+            </button>
+          </div>
         </div>
-        <div id="wrapper">
-          <div id="searchBarDiv">
-            <form action="/" method="get">
-              <input
-                type="text"
-                id="search-bar"
-                placeholder="Search the Archive"
-                name="s"
-                value={searchInput}
-                onChange={(e) => getSearchInput(e.target.value)}
-              />
-            </form>
-            <div id="buttonDiv">
-              <button onClick={MakeLamdaCall} id="searchButton">
-                Search
-              </button>
-            </div>
-          </div>
-          <div id="imgDiv">
-            {imageSrc && (
-              <div>
-                <img src={imageSrc} alt="Img from API" />
-              </div>
-            )}
-          </div>
-          <div>
-            <Link to="/video">
-              <button id="videoPageButton">Video</button>
-            </Link>
-          </div>
+        <div id="imgDiv">
+          {images.map((image) => (
+            <img
+              key={image.id}
+              src={`data:${image.mimetype};base64, ${image.data}`}
+              alt={image.description}
+            />
+          ))}
+        </div>
+        <div>
+          <Link to="/video">
+            <button id="videoPageButton">Video</button>
+          </Link>
         </div>
       </div>
-    );
-  }
-  return RenderPage();
+    </div>
+  );
 };
 
 export default LandingPage;
--- src/Components/VideoPage/videoPage.js
+++ src/Components/VideoPage/videoPage.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./videoStyles.css";
 
 const VideoPage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   function RenderPage() {
     return (
       <div>
         <div id="videoTitleDiv">
           <h1 id="videoTitle">Video Page</h1>
+        </div>
+        <div>
+          <button id="GoBackButton" onClick={handleClick}>
+            Go Back
+          </button>
         </div>
       </div>
     );
